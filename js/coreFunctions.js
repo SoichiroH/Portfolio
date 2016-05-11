7a74078e8a398956b0834bc56993aaa333b34acb
SoichiroH <soichiro.h21@gmail.com>
2016-05-10 20:24:01 -0400

    Added own side-nav responsive feature instead of materialize

--- js/coreFunctions.js
+++ js/coreFunctions.js
@@ -3,6 +3,14 @@
  */
 $(document).ready(function(){
 
+    $(window).resize(function(){
+        if(window.innerWidth < 950) {
+            $("#sideNav").css('transform', 'translateX(-100%)');
+        }else {
+            $("#sideNav").css('transform', 'translateX(0)');
+        }
+    });
+
     $("#downloadResume").click(function () {
         window.location = 'download/Resume-Hirata Soichiro.pdf';
     });
