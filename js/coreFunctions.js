b1decd7452273eb61326bd21b090a202541b747d
SoichiroH <soichiro.h21@gmail.com>
2016-05-13 02:45:06 -0400

    Added top bar transition function and changed color themes

--- js/coreFunctions.js
+++ js/coreFunctions.js
@@ -3,14 +3,36 @@
  */
 $(document).ready(function(){
 
+/*    $("li").hover(function(){
+        $( "." ).animate({ "left": "+=50px" }, "slow" );
+    });*/
+
+    var visible = false;
+
     if(window.innerWidth < 1000) {
         $("#sideNav").css('transform', 'translateX(-100%)');
+        if (!visible){
+            $("#topNav").append("<nav id='newNav' class='nav'><div class='navContainer center'><a class='name'>Soichiro Hirata</a></div></nav>");
+            $("#newName").remove();
+            visible = true;
+        }
     }
     $(window).resize(function(){
         if(window.innerWidth < 1000) {
             $("#sideNav").css('transform', 'translateX(-100%)');
+            if (!visible){
+                $("#topNav").append("<nav id='newNav' class='nav'><div class='navContainer center'><a class='name'>Soichiro Hirata</a></div></nav>");
+                $("#newName").remove();
+                visible = true;
+            }
         }else {
             $("#sideNav").css('transform', 'translateX(0)');
+            $("#newNav").remove();
+            if (visible){
+                $("#bannerName").append("<h1 id='newName' class='center mainFont'>Soichiro Hirata</h1>");
+            }
+
+            visible = false;
         }
     });
 
