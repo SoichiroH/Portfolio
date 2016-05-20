4ea0cb401866fd46d4b0b7fca7968cbf4f77030c
SoichiroH <soichiro.h21@gmail.com>
2016-05-20 16:07:12 -0400

    Fixed page flicker bug on initial page load.

--- js/animations.js
+++ js/animations.js
@@ -2,12 +2,33 @@
  * Created by Soichiro on 5/8/2016.
  */
 
-$(window).load(function(){
-    $('#ajaxLoader').fadeOut(3000);
-    console.log('window load');
-});
+$('header, main, footer').css({'visibility': 'visible'}).fadeOut(0).delay(3000).fadeIn(0).promise().done(function(){bannerAnimation()});
 
-$(document).ready(function(){
+//Banner
+function bannerAnimation(){
+    console.log('start banner animation');
+
+    $("#sideNav").hide().slideDown(1200);
+
+    $("#sideNav").animate({
+        'transform': 'translateX(-100%)'
+    },1000, function(){
+        console.log('call back');
+
+    });
+
+    $('.mainBackground').hide().fadeIn(2500);
+
+    $('#newName').hide().fadeIn(2700);
+    $('#roleTitle').hide().fadeIn(2700);
+
+    $("#bannerPaneResume").hide().animate({ height: 'toggle', opacity: 'toggle' }, 2700);
+    $("#bannerPaneContact").hide().animate({ height: 'toggle', opacity: 'toggle' }, 2700);
+}
+
+
+
+$(function(){
 
 //Sidebar
     //Scroll to each section when sidebar a's are clicked
@@ -16,7 +37,7 @@ $(document).ready(function(){
         id = id.replace("link","");
 
         $('html,body').animate({
-            scrollTop: $("#"+id).offset().top},
+                scrollTop: $("#"+id).offset().top},
             'slow');
     }
 
@@ -26,27 +47,5 @@ $(document).ready(function(){
         e.preventDefault();
         scrollToDiv($(this).attr("id"));
     });
-
-//Banner
-    function bannerAnimation(){
-
-        $("#sideNav").hide().slideDown(1200);
-
-        $("#sideNav").animate({
-            'transform': 'translateX(-100%)'
-        },1000, function(){
-            console.log('call back');
-
-        });
-
-        $('.mainBackground').hide().fadeIn(2500);
-
-        $('#newName').hide().fadeIn(2700);
-        $('#roleTitle').hide().fadeIn(2700);
-
-        $("#bannerPaneResume").hide().animate({ height: 'toggle', opacity: 'toggle' }, 2700);
-        $("#bannerPaneContact").hide().animate({ height: 'toggle', opacity: 'toggle' }, 2700);
-    }
-    bannerAnimation();
 });
 
---
+++ js/loader.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Soichiro on 5/20/2016.
+ */
+
