d8c55b4e317e4e0a1d6f62fc14c874e335452d16
Soichiro Hirata <soichiro.h21@gmail.com>
2016-06-05 01:48:20 -0700

    Performance Optimization: Added shrunk images. Deleting unnecessary files and lines.

--- js/animations.js
+++ js/animations.js
@@ -64,7 +64,7 @@ $(function(){
     $('.mask').on({mouseenter: function(){
             $(this).css({'opacity':'1'});
         },mouseleave: function(){
-            $(this).css({'opacity':'0.1'});
+            $(this).css({'opacity':'0.2'});
         }
     });
 
